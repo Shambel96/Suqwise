3e56be3066af92e88bff878ab2bb0ec813bb8151
Shambel Dechu <shambeldechu@gmail.com>
2025-10-27 16:56:32 +0300

    feat(server):updated server to improve readability

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
-console.log("🚀 Jocode Starter Initialized!");
+const dotenv = require("dotenv");
+const app = require("./app");
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("WELCOME HOME BRUH!!!");
+});
+
+
+const PORT = process.env.PORT || 5555;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});

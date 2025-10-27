522872b94ded8a3b4289c8a62c1030e52d562058
Shambel Dechu <shambeldechu@gmail.com>
2025-10-27 23:12:49 +0300

    fix: db name and username mismatched

--- backend/models/db.js
+++ backend/models/db.js
@@ -4,9 +4,9 @@ require("dotenv").config();
 // Create a shared pool for the application
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "suqwise",
+  user: process.env.DB_USER || "SuqWIse_Hackathon",
   password: process.env.DB_PASSWORD || "123456",
-  database: process.env.DB_NAME || "suqwise",
+  database: process.env.DB_NAME || "SuqWIse_Hackathon",
   waitForConnections: true,
   connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT, 10) || 10,
   queueLimit: 0,
